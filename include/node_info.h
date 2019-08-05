c1d1e007baa94d06414a7e477e7b3585fed95a2e
Taichi Souma <izewfktvy533zjmn@gmail.com>
2019-08-06 02:59:11 +0900

    Update Node Administration Task
    I updated that create_node_info_table function can create with reference to nodeinfo/.init_node_info.

--- include/node_info.h
+++ include/node_info.h
@@ -4,6 +4,8 @@
 #include <netinet/in.h>
 
 #define TABLE_SIZE 30
+#define BUF_SIZE 1025
+#define INIT_NODE_INFO_FILE "nodeinfo/.init_node_info"
 
 
 typedef struct NodeInfo_t {
@@ -13,6 +15,7 @@ typedef struct NodeInfo_t {
 
 
 NodeInfo_t** create_node_info_table();
+void _show_node_info_table(NodeInfo_t** node_info_table);
 void destroy_node_info_table(NodeInfo_t** node_info_table);
 NodeInfo_t* update_node_info(NodeInfo_t* node_info, in_addr_t new_addr, in_port_t port);
 
--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 
 int main(int argc, char* argv[]) {
     NodeInfo_t** node_info_table = create_node_info_table();
+    _show_node_info_table(node_info_table);
     destroy_node_info_table(node_info_table);
 
     return 0;
