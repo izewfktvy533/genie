f768ec4c66f992bd2c58d432f93a5e94c4d74aa2
Taichi Soma <izewfktvy533zjmn@gmail.com>
2019-08-20 23:49:06 +0900

    Add Semaphore

---
+++ study/07_IPC/sem/operate_sem0.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+#define NSEMS 16
+
+
+int main() {
+    key_t key;
+    int semid;
+    struct sembuf sb;
+
+    key = ftok("./test", 1);
+    semid = semget(key, NSEMS, 0600);
+
+    sb.sem_num = 0;
+    sb.sem_op  = -1;
+    sb.sem_flg = SEM_UNDO;
+
+    printf("before semop()\n");
+
+    semop(semid, &sb, 1);
+
+    printf("after semop()\n");
+    printf("press enter to exit\n");
+    getchar();
+
+    return 0;
+}
